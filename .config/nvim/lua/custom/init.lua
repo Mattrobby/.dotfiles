5537753e687b1bf72b59dd66ad2659566c1305d0
Matthew Stefanovic <matthew@stefanovic.us>
2023-07-20 11:46:47 -0400

    added custom lua

---
+++ .config/nvim/lua/custom/init.lua
@@ -0,0 +1,9 @@
+-- local autocmd = vim.api.nvim_create_autocmd
+
+vim.opt.colorcolumn = "80"
+vim.env.VIRTUAL_ENV = "~/Programs/GPT-Responder/GPT-Responder-Backend/.venv"
+-- Auto resize panes when resizing nvim window
+-- autocmd("VimResized", {
+--   pattern = "*",
+--   command = "tabdo wincmd =",
+-- })
---
+++ .config/nvim/lua/custom/mappings.lua
@@ -0,0 +1,33 @@
+---@type MappingsTable
+local M = {}
+
+M.general = {
+  n = {
+    [";"] = { ":", "enter command mode", opts = { nowait = true } },
+    ["<C-h>"] = { "<cmd> TmuxNavigateLeft<CR>", "window left" },
+    ["<C-l>"] = { "<cmd> TmuxNavigateRight<CR>", "window right" },
+    ["<C-j>"] = { "<cmd> TmuxNavigateDown<CR>", "window down" },
+    ["<C-k>"] = { "<cmd> TmuxNavigateUp<CR>", "window up" },
+  },
+}
+
+-- Dap (debugging) keybinds
+M.dap = {
+  plugin = true,
+  n = {
+    ["<leader>db"] = { "<cmd> DapToggleBreakpoint <CR>" },
+  },
+}
+
+M.dap_python = {
+  plugin = true,
+  n = {
+    ["<leader>dpr"] = {
+      function()
+        require("dap_python").test_method()
+      end,
+    },
+  },
+}
+
+return M
