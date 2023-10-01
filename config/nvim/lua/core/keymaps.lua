94014d15640a3ced1f22d81248e9ba1ed5ad23c2
Matthew Stefanovic <ms@stefanovic.us>
2023-10-01 01:08:18 -0400

    Added vim window navigation keybinds

--- config/nvim/lua/core/keymaps.lua
+++ config/nvim/lua/core/keymaps.lua
@@ -15,3 +15,9 @@ vim.opt.shiftround = true
 vim.opt.expandtab = true
 
 vim.keymap.set('n', '<esc>', ':noh<CR>:echo ""<CR>', {noremap = true, silent = true})
+
+-- Vim Window Navigation
+vim.api.nvim_set_keymap('n', '<C-h>', '<C-w>h', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-j>', '<C-w>j', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-k>', '<C-w>k', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-l>', '<C-w>l', { noremap = true, silent = true })
