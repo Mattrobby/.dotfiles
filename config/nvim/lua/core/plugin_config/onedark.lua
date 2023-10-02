fa548bb5cd320af8c555ab653b6883c3eaa2747a
Matthew Stefanovic <ms@stefanovic.us>
2023-10-02 14:46:22 -0400

    Updated OneDark Config Settings

--- config/nvim/lua/core/plugin_config/onedark.lua
+++ config/nvim/lua/core/plugin_config/onedark.lua
@@ -8,7 +8,7 @@ require('onedark').setup  {
     cmp_itemkind_reverse = false, -- reverse item kind highlights in cmp menu
 
     -- toggle theme style ---
-    toggle_style_key = nil, -- keybind to toggle theme style. Leave it nil to disable it, or set it to a string, for example "<leader>ts"
+    toggle_style_key = '<leader>ts', -- keybind to toggle theme style. Leave it nil to disable it, or set it to a string, for example "<leader>ts"
     toggle_style_list = {'dark', 'darker', 'cool', 'deep', 'warm', 'warmer', 'light'}, -- List of styles to toggle between
 
     -- Change code style ---
@@ -16,7 +16,7 @@ require('onedark').setup  {
     -- You can configure multiple style with comma separated, For e.g., keywords = 'italic,bold'
     code_style = {
         comments = 'italic',
-        keywords = 'none',
+        keywords = 'italic',
         functions = 'none',
         strings = 'none',
         variables = 'none'
