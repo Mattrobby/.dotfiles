1887ecc1d05338003e7d90d0f6b39b9ffb20409f
Matthew Scott Stefanovic <matthew@stefanovic.us>
2024-08-15 13:25:01 -0400

    Moved where fonts are stored to work with stow

--- .config/zsh/path.zsh
+++ .config/zsh/path.zsh
@@ -1,3 +1,4 @@
 export PATH="$HOME/.local/bin:$PATH"
+export PATH="$(go env GOPATH)/bin:$PATH"
 export SSH_AUTH_SOCK=$XDG_RUNTIME_DIR/ssh-agent.socket
 source /usr/share/nvm/init-nvm.sh >> ~/.bashrc
